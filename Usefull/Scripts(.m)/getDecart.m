33e198867180eab25e9bada5d23c922ad082bbf3
Roman <suvorov_roman_01@mail.ru>
2019-09-28 23:53:40 +0600

    TOE 2 lab

--- Usefull/Scripts(.m)/getDecart.m
+++ Usefull/Scripts(.m)/getDecart.m
@@ -2,7 +2,6 @@ function imageVar = getDecart(radius,angle)
   realPart = radius*cosd(angle);
   imagePart = radius*sind(angle);
   imageVar = realPart + imagePart*i
-endfunction
 %Задавать угол нужно в градусах.
 %Пример использования: getDecart(5,45);
 
