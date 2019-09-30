62cb5aac3f6432ec15b74aed79618e2e5701d0ef
Roman <suvorov_roman_01@mail.ru>
2019-10-01 00:46:56 +0600

    TOE lab2

---
+++ Usefull/Scripts(.m)/imag2plot.m
@@ -0,0 +1,18 @@
+function imag2plot(arr, leg)
+hold off
+for n = 1:length(arr)
+    rvar = real(arr{n});
+    ivar = imag(arr{n});
+    plot([0 rvar],[0 ivar], '-*')
+    hold on
+end
+for m = 1:length(leg)
+    text(real(arr{m})+.2,imag(arr{m})+.2,leg{m});
+    hold on
+end
+%legend('boxoff');
+grid on
+xlabel('real')
+ylabel('imag')
+hold off
+end
