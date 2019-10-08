fda2ecbc979be389200e9dd13a9b861eb1dcdd65
Roman <suvorov_roman_01@mail.ru>
2019-10-09 01:32:02 +0600

    commits

--- Usefull/Scripts(.m)/getDecart.m
+++
@@ -1,7 +0,0 @@
-function imageVar = getDecart(radius,angle)
-  realPart = radius*cosd(angle);
-  imagePart = radius*sind(angle);
-  imageVar = realPart + imagePart*i
-%Задавать угол нужно в градусах.
-%Пример использования: getDecart(5,45);
-
