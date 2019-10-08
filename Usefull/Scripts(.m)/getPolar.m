3c79e10b71cec97ed72d27147e7de80b0b7c99fb
Roman <suvorov_roman_01@mail.ru>
2019-10-09 01:32:02 +0600

    commits

--- Usefull/Scripts(.m)/getPolar.m
+++
@@ -1,5 +0,0 @@
-function [radius,angle] = getPolar(imageVar)
-  radius = sqrt(real(imageVar).^2+imag(imageVar).^2)
-  angle = atand(imag(imageVar)./real(imageVar))
-%Просто вписываете своё комплексное число. Обязательно используйте i, а не j.
-%Пример использования: getPolar(1+1*i);
